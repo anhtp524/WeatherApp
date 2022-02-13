f505f481782db6d601d156bebe6edd0fbc87c5fc
anhtp524 <61215691+anhtp524@users.noreply.github.com>
2022-02-13 09:20:00 +0700

    first cmt

---
+++ WeatherApp/app.js
@@ -0,0 +1,16 @@
+// var search = document.querySelector('.search')
+// var city = document.querySelector('.city')
+// var country = document.querySelector('.country')
+// var value = document.querySelector('.value')
+// var shortDesc = document.querySelector('.short-desc')
+// var visibility = document.querySelector('.visibility span')
+// var wind = document.querySelector('.wind span')
+// var humidity = document.querySelector('.humidity span')
+
+// function changeWeatherUI() {
+//     search.value.trim()
+//     let apiURL = 'api.openweathermap.org/data/2.5/weather?q=hanoi&appid=46d7e38209466c990ed6015e1ab233d9'
+//     fetch()
+// }
+
+console.log("Chúc mừng năm mới. Chúc gia đình, anh em, bạn bè năm mới luôn luôn mạnh khỏe, hạnh phúc và thành công!!! <3")
