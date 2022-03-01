8030ecc54906481c45f4c11b34043ed2b74a6412
anhtp524 <61215691+anhtp524@users.noreply.github.com>
2022-03-01 23:18:06 +0700

    finally

--- WeatherApp/app.js
+++ WeatherApp/app.js
@@ -1,16 +1,38 @@
-// var search = document.querySelector('.search')
-// var city = document.querySelector('.city')
-// var country = document.querySelector('.country')
-// var value = document.querySelector('.value')
-// var shortDesc = document.querySelector('.short-desc')
-// var visibility = document.querySelector('.visibility span')
-// var wind = document.querySelector('.wind span')
-// var humidity = document.querySelector('.humidity span')
+var search = document.querySelector('.search')
+var city = document.querySelector('.city')
+var country = document.querySelector('.country')
+var value = document.querySelector('.value')
+var shortDesc = document.querySelector('.short-desc')
+var visibility = document.querySelector('.visibility span')
+var wind = document.querySelector('.wind span')
+var humidity = document.querySelector('.humidity span')
+var value = document.querySelector('.value')
+var time = document.querySelector('.time')
 
-// function changeWeatherUI() {
-//     search.value.trim()
-//     let apiURL = 'api.openweathermap.org/data/2.5/weather?q=hanoi&appid=46d7e38209466c990ed6015e1ab233d9'
-//     fetch()
-// }
+function changeWeatherUI(city_var) {
+    
+    let apiURL = `http://api.openweathermap.org/data/2.5/weather?q=${city_var}&appid=46d7e38209466c990ed6015e1ab233d9`
+    fetch(apiURL)
+        .then(response => response.json())
+        .then(function(data){
+            city.innerText = data.name;
+            country.innerText = data.sys.country;
+            value.innerText = Math.floor((data.main.temp - 273))
+            visibility.innerText = data.visibility + 'm';
+            wind.innerText = data.wind.speed + 'm/s';
+            humidity.innerText = data.main.humidity + 'm/s';
+            shortDesc.innerText = data.weather[0].main
+            time.innerText = new Date().toLocaleString()
+        })
+}
 
-console.log("Chúc mừng năm mới. Chúc gia đình, anh em, bạn bè năm mới luôn luôn mạnh khỏe, hạnh phúc và thành công!!! <3")
+search.onkeypress = function(e){
+    if(e.code === 'Enter') {
+        let city_var = search.value.trim()
+        changeWeatherUI(city_var)
+    }
+}
+changeWeatherUI('Hanoi');
+
+
+
